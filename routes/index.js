2e6bc81ef5d151be7a2b4ebdc84ccbb8adb66a60
Tim Dose <tim@timdose.com>
2014-11-15 18:21:02 -0500

    basic routes setup

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/user/:user', function(req, res ) {
+    var data = {user:req.params.user}
+    res.render('user', data );
 });
 
 module.exports = router;
