27c9d270da5ce0d85040f607f306ac99b7d919de
Tim Dose <tim@timdose.com>
2014-11-16 12:40:06 -0500

    working with the github api node module

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var https = require('https');
+var GitHubApi = require('github');
+
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -7,8 +11,55 @@ router.get('/', function(req, res) {
 });
 
 router.get('/user/:user', function(req, res ) {
-    var data = {user:req.params.user}
-    res.render('user', data );
+    var github = new GitHubApi({
+        version: '3.0.0',
+        debug:true,
+        protocol: 'https',
+        host: 'api.github.com',
+        timeout: 5000
+    });
+    github.events.getFromUser({
+        user: req.params.user
+    }, function(err, response) {
+        res.send(response);
+    });
 });
+
+// router.get('/user/:user', function(req, res ) {
+//     var body = '';
+
+//     // https.globalAgent.options.secureProtocol = 'SSLv3_method';
+
+//     var options = {
+//         host: 'api.github.com',
+//         path: '/users/:user/repos'.replace(':user', req.params.user),
+//         method: 'GET',
+//         headers: {
+//             'host': 'api.github.com',
+//             // 'user-agent':'github-usage',
+//             'content-length': '0'
+//         },
+//         port: 443
+//     }
+
+//     console.log('fetching data from ' + options.host + options.path );
+    
+//     var request = https.request(options, function(response) {
+//         response.setEncoding('utf8');
+
+//         response.on('data', function(chunk) {
+//             body += chunk;
+//         });
+
+//         response.on('end', function() {
+//             console.log(body);
+//             var data = JSON.parse(body);
+//             res.render('user', data );
+//         });
+//     })
+//     .on('error', function (e) {
+//         console.log('Error:', e);
+//     });
+// });
 
 module.exports = router;
