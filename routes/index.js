b4d90826529a6c276b8549996d63177c5af96631
Tim Dose <tim@timdose.com>
2014-11-16 14:05:54 -0500

    pulled github module back in

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var https = require('https');
+var GithubApi = require('github');
 
 var router = express.Router();
 
+var github = new GithubApi({
+    version: '3.0.0',
+    debug: true
+});
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -10,44 +15,13 @@ router.get('/', function(req, res) {
 });
 
 router.get('/user/:user', function(req, res ) {
-    var body = '';
-
-    var options = {
-        host: 'api.github.com',
-        path: '/users/:user/events'.replace(':user', req.params.user),
-        method: 'get',
-        headers: { 
-            host: 'api.github.com',
-            'content-length': '0',
-            'user-agent': 'github-usage',
-            accept: 'application/vnd.github.v3+json' 
-        },
-        port: 443
-    }
-
-    console.log('fetching data from ' + options.host + options.path );
-    
-    var request = https.request(options, function(response) {
-        response.setEncoding('utf8');
-
-        response.on('data', function(chunk) {
-            body += chunk;
-        });
-
-        response.on('end', function() {
-            console.log(body);
-            var data = {
-                user: req.params.user,
-                activity: body
-            }
-            res.render('user', data );
-        });
-    })
-    .on('error', function (e) {
-        console.log('Error:', e);
+    github.repos.getFromUser({user:req.params.user}, function(err, response ) {
+        var data = {
+            user: req.params.user,
+            repos: JSON.stringify(response)
+        }
+        res.render('user', data);
     });
-
-    request.end();
 });
 
 module.exports = router;
