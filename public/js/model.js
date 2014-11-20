d4d540f301477afbd6e0dc3e1072cdad2bc53a9e
Tim Dose <tim@timdose.com>
2014-11-19 22:08:12 -0500

    move data manipulation to 'model'

---
+++ public/js/model.js
@@ -0,0 +1,62 @@
+
+var Model = new function() {
+    var self = this;
+
+    self.fetchData = function (user, callback) {
+        $.ajax({
+            url: '/api/user/' + user,
+            dataType: 'json'
+        })
+        .done( function( data ) {
+            callback(data);
+        })
+        .error( function(err) {
+            console.log(err);
+        });
+    }
+
+    self.getCommitsPerRepo = function(data) {
+        var repos = data.repos.map(function(repo){
+            return { 
+                name:repo.name, 
+                numCommits: repo.numCommits,
+                popularity: repo.watchers_count + repo.stargazers_count + repo.forks_count
+            }
+        })
+        .filter(function(repo) {
+            return repo.numCommits > 0;
+        });
+        return repos;
+    }
+
+    self.getCommitsPerWeek = function(data) {
+        var commits = [];
+
+        data.repos.forEach(function(repo){
+            Array.prototype.push.apply(commits, repo.commits);
+        });
+
+
+        var weeks = d3.nest()
+            .key(function(commit){
+                var d = new Date(commit.commit.author.date);
+                var day = d.getDay();
+                var sunday = new Date(d.setDate(d.getDate()-day));
+                var month = ('00' + (sunday.getMonth()+1)).substr(-2,2);
+                var date = ('00' + sunday.getDate()).substr(-2,2);
+                var formatted = [sunday.getFullYear(),month,date].join('-')
+                return formatted;
+            })
+            .rollup(function(values) { return values.length; })
+            .entries(commits)
+            .map(function(week) {
+                return {name:week.key, numCommits: week.values };
+            })
+            .sort(function(a, b) {
+                return new Date(a.name) - new Date(b.name);
+            });
+
+        return weeks;
+    }
+
+}
